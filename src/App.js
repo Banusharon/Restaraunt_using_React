69090f90bfb88c03abf89f782a63614a1688fcf0
Banusharon <banusharon2000@gmail.com>
2023-07-23 15:46:10 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 function App() {
   return (
